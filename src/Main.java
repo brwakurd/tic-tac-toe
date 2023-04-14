74daa623a7f819c440463880cfa19f40ed169f46
Brwa <59148836+brwakurd@users.noreply.github.com>
2023-04-14 13:17:07 +0300

    Revert "final"
    This reverts commit b1e2e9ed4ca116ea842fa1b885e2886e46eabc04.

--- src/Main.java
+++ src/Main.java
@@ -2,80 +2,225 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    // Stats for users.
     public static void main(String[] args) {
-        int[][] studentsPrototype = new int[5][3];
-        int[][] students = readArray(studentsPrototype);
-
-        int[] avg = avgCalc(students);
-        int percentageOfSuccess = thirdExam(students);
-        int maxAvg = maximumAverage(avg);
-        System.out.println("The maximum/highest average is " + maxAvg + "%.");
-        System.out.println("Percentage of studens who have passed the third exam is " + percentageOfSuccess + " %.");
-
-        // the fifth question is asking to print all the elements using array overloading if you don't know whats that read this link https://www.mygreatlearning.com/blog/method-overloading-in-java/#:~:text=Method%20overloading%20in%20java%20is,name%2C%20but%20with%20different%20parameters.
-        printElements(students);
-        printElements(avg);
+        game_start();
     }
 
-    static void printElements(int[][] twoDimensional) {
-        System.out.println("Printing two dimensionals..");
-        for (int i = 0; i < twoDimensional.length; i++) {
-            for (int j = 0; j < twoDimensional[i].length; j++) {
-                System.out.print(twoDimensional[i][j] + " ");
+    static void game() {
+        int [][] data = {
+
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        boolean play = true;
+        int [][] player1 = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        };
+        int [][] player2 = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        };
+
+        display_game(data);
+        int win = 0;
+        int winner = 0;
+        while (play == true) {
+            System.out.println("Player 1, choose tile:  ");
+            int [] temp = {10, 10};
+
+            int num = player_choice();
+            temp = row_logic(num);
+            if (data[temp[0]][temp[1]] == 11 || data[temp[0]][temp[1]] == 10) {
+                while (true) {
+                    System.out.println("That place is taken already, try again.");
+                    num = player_choice();
+                    temp = row_logic(num);
+                    if (!(data[temp[0]][temp[1]] == 11 || data[temp[0]][temp[1]] == 10)) {
+                        break;
+                    }
+                }
             }
-            System.out.println();
+
+            data[temp[0]][temp[1]] = 11;
+            player1[temp[0]][temp[1]] = 1;
+
+            win = game_logic(player1);
+
+            if (draw_logic(data)) {
+                winner = 3;
+                break;
+            }
+            if (win == 1) {
+                play = false;
+                winner = 1;
+                break;
+            }
+
+            display_game(data);
+
+            System.out.println("Player 2, choose tile:  ");
+            int num2 = player_choice();
+            temp = row_logic(num2);
+            if (data[temp[0]][temp[1]] == 11 || data[temp[0]][temp[1]] == 10) {
+                while (true) {
+                    num2 = player_choice();
+                    temp = row_logic(num2);
+                    if (data[temp[0]][temp[1]] != 11 || data[temp[0]][temp[1]] != 10) {
+                        break;
+                    }
+                }
+            }
+            data[temp[0]][temp[1]] = 10;
+            player2[temp[0]][temp[1]] = 1;
+
+            win = game_logic(player2);
+
+            if (draw_logic(data)) {
+                winner = 3;
+                break;
+            }
+            if (win == 1) {
+                play = false;
+                winner = 2;
+                break;
+            }
+
+            display_game(data);
+
+        }
+        display_game(data);
+        if (winner == 3) {
+            System.out.println("Draw! No one won.");
+        } else {
+            System.out.println("Player " + winner + " won!");
+        }
+
+        game_start();
+    }
+
+    static boolean draw_logic(int[][] data) {
+        int counter = 0;
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                if (data[i][j] == 11 || data[i][j] == 10) {
+                    counter += 1;
+                }
+            }
+        }
+
+        if (counter >= 9) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    static int player_choice() {
+        Scanner int_input = new Scanner(System.in);
+        int num = int_input.nextInt();
+        return num;
+    }
+    static int game_logic(int[][] player) {
+        for (int i = 0; i <= 2; i++) {
+            if (player[i][0] == 1 && player[i][1] == 1 && player[i][2] == 1) {
+                System.out.println("ues1");
+                return 1;
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            if (player[0][i] == 1 && player[1][i] == 1 && player[2][i] == 1) {
+                System.out.println("ues2");
+                return 1;
+            }
+        }
+        if (player[0][0] == 1 && player[1][1] == 1 && player[2][2] == 1) {
+            System.out.println("ues3");
+            return 1;
+        } else if (player[0][2] == 1 && player[1][1] == 1 && player[2][0] == 1) {
+            System.out.println("ues4");
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+
+    static int[] row_logic(int num) {
+        int [] return_temp = {10, 10};
+        if (num == 1 || num == 2 || num == 3){
+            return_temp[0] = 0;
+            return_temp[1] = num- 1;
+            return return_temp;
+        } else if (num == 4 || num == 5 || num == 6){
+            return_temp[0] = 1;
+            return_temp[1] = num - 4;
+            return return_temp;
+        } else if (num == 7 || num == 8 || num == 9){
+            return_temp[0] = 2;
+            return_temp[1] = num - 7;
+            return return_temp;
+        } else {
+            return return_temp;
         }
     }
 
-    static void printElements(int[] oneDimensional) {
-        System.out.println("Printing one dimensional...");
-        for (int i = 0; i < oneDimensional.length; i++) {
-            System.out.print(oneDimensional[i]);
+    static void game_start() {
+        System.out.print("Press 1 if you want to start the game:  ");
+        Scanner sc = new Scanner(System.in);
+        int decide = sc.nextInt();
+        if (decide == 1) {
+            System.out.println("Game starting...");
+            game();
         }
-    }
-    static int maximumAverage(int[] avg) { // Maximum average means, the highest average in the student (like by one student) zortrin m3adal // class nerd
-        int maxAvg = avg[0];
-        for (int i = 0; i < avg.length; i++) {
-            if (avg[i] > maxAvg) {
-                maxAvg = avg[i];
-            }
-        }
-        //
-        return maxAvg;
     }
 
-    static int thirdExam(int[][] students) {
-        int percentageAmt = 100 / students.length;
-        int percentageOfSuccess = 0;
-        for (int i = 0; i < students.length; i++) {
-            if (students[i][2] > 50) {
-                percentageOfSuccess += percentageAmt;
-            }
-        }
-        return percentageOfSuccess;
+    public static void display_game(int [][] data) {
+        int vertical_size = 5;
+        int space = 12;
+
+        System.out.println("___________________________________________");
+        display_tiles(space, vertical_size, " ", data[0]);
+        display_tiles(space, vertical_size, " ", data[1]);
+        display_tiles(space, vertical_size, " ", data[2]);
     }
 
-    static int[] avgCalc(int[][] students) {
-        int[] avg = new int[students.length];
-        for (int i = 0; i < students.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < students[i].length; j++) {
-                sum += students[i][j];
-            }
-            avg[i] = sum / students[i].length;
-        }
-        return avg;
-    }
-    static int[][] readArray(int[][] students) {
-        Scanner s = new Scanner(System.in);
+    static void display_tiles(int space, int size, String filler, int [] data) {
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j <= 2; j++) {
+                System.out.print("|");
+                if (i == size) {
+                    filler = "_";
+                }
+                if (i == (size/2)) {
+                    for (int d = 0; d <= space; d++) {
+                        if (d == (space/2)) {
+                            switch(data[j]) {
+                                case 11:
+                                    System.out.print("x");
+                                    break;
+                                case 10:
+                                    System.out.print("o");
+                                    break;
+                                default:
+                                    System.out.print(data[j]);
+                            }
+                        }
+                        else {
+                            System.out.print(filler);
+                        }
+                    }
+                } else {
+                    for (int d = 0; d <= space; d++) {
+                        System.out.print(filler);
+                    }
+                }
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("Enter the grades for student " + (i+1));
-            for (int j = 0; j < students[i].length; j++) {
-                students[i][j] = s.nextInt();
             }
+            System.out.println("|");
         }
-        return students;
     }
 }
